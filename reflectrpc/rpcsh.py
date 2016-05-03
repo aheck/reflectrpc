7dcb010894c57b2bcacf90c457dc6f094fb95641
Andreas Heck <aheck@gmx.de>
2016-05-03 22:00:30 +0200

    Fixes unnecessary line breaks

--- reflectrpc/rpcsh.py
+++ reflectrpc/rpcsh.py
@@ -124,12 +124,10 @@ class ReflectRpcShell(Cmd):
         if self.service_description:
             self.intro += "\n\nSelf-description of Service:\n============================\n"
             if self.service_description['name']:
-                print()
                 self.intro += self.service_description['name']
                 if self.service_description['version']:
                     self.intro += " (%s)\n" % (self.service_description['version'])
             if self.service_description['description']:
-                print()
                 self.intro += self.service_description['description']
 
     def enable_tls(self):
